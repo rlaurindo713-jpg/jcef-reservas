1d49b56a444dc01890786113acd507cc0d0203b0
rlaurindo713-jpg <rlaurindo713@gmail.com>
2026-06-02 10:04:23 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'jcf-reservas-v1';
+const FILES = [
+  '/reservas-jcf/',
+  '/reservas-jcf/index.html',
+  '/reservas-jcf/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
